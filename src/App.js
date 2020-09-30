f0a259da80d125d7b3837eda193948a7b3dce026
fledith <barateon023@gmail.com>
2020-10-01 00:08:14 +0300

    auth

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import './App.css';
 import UsersContainer from './components/Users/UsersContainer';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
 import Music from './components/Music/Music';
 import Navbar from './components/Navbar/Navbar';
 import News from './components/News/News';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import Settings from './components/Settings/Settings';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 
 const App = () => {  
     return ( 
         <div className="app-wrapper">
-            <Header />
+            <HeaderContainer />
             <Navbar />
             <div className="app-wrapper-content">
                 <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,17 @@ import logo from './../../img/logo_new2.png';
 import z from './Header.module.css';
 
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={z.header}>
             <div className={z.logo}>
                 <NavLink to="/profile">
                     <img src={logo} alt='logo'/>
                 </NavLink>
+            </div>
+            <div className={z.loginBlock}>
+                { props.isAuth ? props.login
+                    : <NavLink to={'/login'}> Login </NavLink>}
             </div>
         </header>
     );
